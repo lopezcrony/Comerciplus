ad5769d819ec603bb8a65ac50b94cfe70d2200f2
lopezcrony <lopezcrony@gmail.com>
2024-08-26 06:33:44 -0500

    Creación de módulos faltantes

---
+++ FRONT-END/src/app/modules/return-sale/return-sale.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-return-sale',
+  standalone: true,
+  imports: [],
+  templateUrl: './return-sale.component.html',
+  styleUrl: './return-sale.component.css'
+})
+export class ReturnSaleComponent {
+
+}
