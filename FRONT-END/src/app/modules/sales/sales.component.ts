5fb458904a1407526437bbdc2b7c7574564bae2b
lopezcrony <lopezcrony@gmail.com>
2024-09-09 19:18:43 -0500

    Avance ventas

--- FRONT-END/src/app/modules/sales/sales.component.ts
+++ FRONT-END/src/app/modules/sales/sales.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
-  selector: 'app-sales',
-  standalone: true,
-  imports: [],
+  selector: 'app-venta-productos',
+  standalone:true,
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './sales.component.html',
-  styleUrl: './sales.component.css'
+  styleUrls: ['./sales.component.css']
 })
 export class SalesComponent {
+  productos = [
+    { nombre: 'Producto 1', precio: 10.99, cantidad: 1 },
+    { nombre: 'Producto 2', precio: 15.50, cantidad: 2 },
+    { nombre: 'Producto 3', precio: 8.75, cantidad: 1 }
+  ];
 
+  creditoAsignado = 0;
+  imprimirRecibo = false;
+
+  incrementarCantidad(producto: any) {
+    producto.cantidad++;
+  }
+
+  decrementarCantidad(producto: any) {
+    if (producto.cantidad > 1) {
+      producto.cantidad--;
+    }
+  }
+
+  eliminarProducto(index: number) {
+    this.productos.splice(index, 1);
+  }
+
+  finalizarVenta() {
+    console.log('Venta finalizada');
+    console.log('Crédito asignado:', this.creditoAsignado);
+    console.log('Imprimir recibo:', this.imprimirRecibo);
+  }
 }
