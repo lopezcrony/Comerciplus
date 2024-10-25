c319402570cb0cf08578de1c39da9a5934284074
Dm29015 <dmcorrea670@soy.sena.edu.co>
2024-10-24 23:13:16 -0500

    Overlay de Perfil

--- FRONT-END/src/app/layout/topbar/topbar.component.ts
+++ FRONT-END/src/app/layout/topbar/topbar.component.ts
@@ -1,27 +1,65 @@
+// topbar.component.ts
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
-import { RouterModule, Router } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
 import { AuthService } from '../../auth/auth.service';
+import { User } from '../../modules/users/users.model';
+import { RolesService } from '../../modules/roles/roles.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [
+    RouterModule, 
+    CommonModule, 
+    OverlayPanelModule,
+    ButtonModule,
+    AvatarModule
+  ],
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+  styleUrls: ['./topbar.component.css'],
 })
-export class TopbarComponent {
-  @Output() toggleSidebar = new EventEmitter<void>();
 
-  constructor(private authService: AuthService) {}
+export class TopbarComponent implements OnInit {
+  @Output() toggleSidebar = new EventEmitter<void>();
+  userData!: any;
+
+  constructor(
+    private authService: AuthService, 
+    private roleService: RolesService
+  ) {}
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    forkJoin({
+      user: this.authService.getUserData(),
+      roles: this.roleService.getAllRoles()
+    }).subscribe({
+      next: ({ user, roles }) => {
+        const userRole = roles.find(r => r.idRol === user.idRol);
+        this.userData = {
+          ...user,
+          nombreRol: userRole ? userRole.nombreRol : 'Rol no encontrado'
+        };
+      },
+      error: (error) => {
+        console.error('Error loading data:', error);
+      }
+    });
+  }
 
   toggleSidebarVisibility() {
     this.toggleSidebar.emit();
   }
 
   logout() {
-    // Eliminar el token y los datos del usuario del almacenamiento local
     this.authService.logout();
     this.preventBack();
   }
