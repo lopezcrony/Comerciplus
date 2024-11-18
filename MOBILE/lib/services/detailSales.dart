a7e59021ec17ecdb74ef66199b381f137f1c3fde
Crony <lopezcrony@gmail.com>
2024-11-18 09:33:58 -0500

    Limpieza de la movil

--- MOBILE/lib/services/detailSales.dart
+++ MOBILE/lib/services/detailSales.dart
@@ -17,24 +17,4 @@ class DetailSaleService {
     }
   }
 
-  // Método para obtener todos los detalles de venta
-  // Future<List<Detailsale>> getSaleDetails() async {
-  //   final url = Uri.parse('$_salesUrl/detallesVenta'); // Ajusta la URL a la de tu endpoint
-
-  //   try {
-  //     final response = await http.get(url);
-
-  //     // Verifica si la respuesta es exitosa
-  //     if (response.statusCode == 200) {
-  //       // Decodifica la respuesta en una lista de detalles de venta
-  //       final List<dynamic> data = json.decode(response.body);
-  //       // Convierte la respuesta en objetos de tipo Detailsale
-  //       return data.map((item) => Detailsale.fromJson(item)).toList();
-  //     } else {
-  //       throw Exception('Error al cargar los detalles de venta service');
-  //     }
-  //   } catch (e) {
-  //     throw Exception('Error al conectar con la API: $e');
-  //   }
-  // }
 }
