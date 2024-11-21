ff4d78d0efc8dd6997d2cce218d835440d672277
Dm29015 <dmcorrea670@soy.sena.edu.co>
2024-11-20 23:02:07 -0500

    Ruta de despliegue

--- FRONT-END/src/environments/environment.ts
+++ FRONT-END/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3006',
+    // apiUrl: 'http://localhost:3006',
+    apiUrl:'https://comerciplus-dggj.onrender.com'
   };
   
