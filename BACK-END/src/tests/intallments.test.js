7b31a660f05c56da029e56c0c1b439d3bd43254b
Dm29015 <dmcorrea670@soy.sena.edu.co>
2024-11-21 16:05:26 -0500

    abonos

--- BACK-END/src/tests/intallments.test.js
+++ BACK-END/src/tests/intallments.test.js
@@ -1,21 +1,33 @@
 const request = require('supertest');
 const app = require('../../main');
 
-jest.setTimeout(20000);
+jest.setTimeout(300000);
 
 describe('GET /abonos', () => {
     let server;
 
     beforeAll(async () => {
-        server = await app;
+        jest.setTimeout(30000);
+        try {
+            server = await app;
+        } catch (error) {
+            console.error('Error al iniciar el servidor:', error);
+            throw error;
+        }
     });
 
     test('Debería retornar un array con los abonos según el crédito', async () => {
-        const res = await request(server).get('/abonos/2');
+        const res = await request(server).get('/abonos/1');
         console.log('Response status:', res.statusCode);
         console.log('Response body:', res.body);
         expect(res.statusCode).toBe(200);
         expect(res.body).toBeInstanceOf(Array);
+    });
+
+    afterAll(async () => {
+        if (server) {
+            await server.close();
+        }
     });
 
 });
