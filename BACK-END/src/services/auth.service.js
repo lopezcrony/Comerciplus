d5aa043e916260397241e02b99b886815dba47fb
Dm29015 <dmcorrea670@soy.sena.edu.co>
2024-11-20 20:26:19 -0500

    node_modules

--- BACK-END/src/services/auth.service.js
+++ BACK-END/src/services/auth.service.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const userRepository = require('../repositories/users.repository');
 
 const SECRET_KEY = process.env.SECRET_KEY;
